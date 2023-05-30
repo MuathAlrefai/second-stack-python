5027b65b4b5c4427318ebaa47b8a9143e48cf9b3
Muath <rigahd@gmail.com>
2023-05-30 13:58:19 +0300

    week 2 tuesday

--- project_2/time_display/views.py
+++ project_2/time_display/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from time import gmtime, strftime
+from time import strftime
 
 def currentTime(request):
     time_dic = {
-        "time": strftime("%Y-%m-%d %I:%M %p", gmtime())
+        "time": strftime("%Y-%m-%d %I:%M %p")
     }
     return render(request, 'index.html', time_dic)
