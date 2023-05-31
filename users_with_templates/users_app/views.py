c1c9c03839dc5f195f7cb545a1bee198d413ab3e
Muath <rigahd@gmail.com>
2023-05-31 23:08:16 +0300

    week 2 thursday

---
+++ users_with_templates/users_app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from .models import Users
+
+def index(request):
+    users = Users.objects.all()
+    users_dict = {
+        "users": list(users.values())
+    }
+    return render(request, 'index.html', users_dict)
+def addUser(request):
+    first_name = request.POST['firstName']
+    last_name = request.POST['lastName']
+    email_address = request.POST['emailAddress']
+    user_age = request.POST['userAge']
+    Users.objects.create(first_name = first_name, last_name = last_name, email_address = email_address, age = user_age)
+    return redirect('/')
