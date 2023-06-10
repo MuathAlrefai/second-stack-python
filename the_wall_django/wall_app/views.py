83fe4955595a11c8465d20bad68225b4c976daba
MuathAlrefai <rigahd@gmail.com>
2023-06-11 00:07:05 +0300

    week 3 thursday

--- the_wall_django/wall_app/views.py
+++ the_wall_django/wall_app/views.py
@@ -1,28 +1,26 @@
 from django.shortcuts import render, redirect
 from login_app.models import User
 from .models import Post, Comment
+from . import models
 
 def make_post(request):
-    user = User.objects.get(id=request.session['userid'])
-    post = request.POST['user_post']
-    Post.objects.create(user = user, post = post)
+    models.make_post_model(request)
     return redirect('/wall')
 
 def make_comment(request, ID):
-    user = User.objects.get(id = request.session['userid'])
-    post = Post.objects.get(id = ID)
-    comment = request.POST['user_comment']
-    Comment.objects.create(post = post, user = user, comment = comment)
+    models.make_comment_model(request, ID)
     return redirect('/wall')
 
+# render the wall.html page on the browser
 def wall(request):
     context = {
-        "user": User.objects.get(id=request.session['userid']),
-        "posts": Post.objects.all().order_by("-created_at"),
+        "user": models.user_session_model(request),
+        "posts": models.get_posts_model(),
         # "comments": Comment.objects.all().order_by("-created_at")
     }
     return render(request, 'wall.html', context)
 
+# delete a post but only if you're the creator of this certain post
 def delete_post(request):
     post = Post.objects.get(id = request.POST['delete_post'])
     user = User.objects.get(id = request.session['userid'])
